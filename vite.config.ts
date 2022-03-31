0bbea5d267c3685bec8eb0d13c479c24f0860146
Yuri Mikhin <urasergeevich@mail.ru>
2022-03-31 18:39:58 +0300

    Enable server proxy in Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,21 @@ export default defineConfig({
     modules: {
       localsConvention: 'camelCase'
     }
+  },
+
+  server: {
+    proxy: {
+      '/logux': {
+        target: 'ws://127.0.0.1:31337',
+        changeOrigin: true,
+        ws: true,
+        rewrite: path => path.replace(/^\/logux/, '')
+      },
+      '/api': {
+        target: 'http://127.0.0.1:31337',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   }
 })

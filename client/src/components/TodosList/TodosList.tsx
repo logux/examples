4a10ac0d7a5f54ccf7ee5dee484b4d94b031aa2a
Yuri Mikhin <urasergeevich@mail.ru>
2022-04-01 12:40:33 +0300

    Move TodoListItem to separate React component.

--- client/src/components/TodosList/TodosList.tsx
+++ client/src/components/TodosList/TodosList.tsx
@@ -1,12 +1,8 @@
 import cn from 'classnames'
 import { useClient, useFilter } from '@logux/client/react'
-import {
-  changeSyncMapById,
-  createSyncMap,
-  deleteSyncMapById
-} from '@logux/client'
+import { createSyncMap } from '@logux/client'
 import { nanoid } from 'nanoid'
-import { useCallback, useEffect, useState, KeyboardEvent } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useStore } from '@nanostores/react'
 
 import { ControlPanel } from '../ControlPanel/ControlPanel'
@@ -14,13 +10,13 @@ import { TextInput } from '../TextInput/TextInput'
 import { ToggleAction } from '../ToggleAction/ToggleAction'
 import { tasksStore } from '../../stores/tasks'
 import { Filter, filterStore } from '../../stores/filter'
+import { TodosListItem } from './TodosListItem'
 import styles from './TodosList.module.css'
 
 export const TodosList = (): JSX.Element => {
   const client = useClient()
   const filter = useStore(filterStore)
   const [editableItemId, setEditableItemId] = useState('')
-  const [editableInitialValue, setEditableInitialValue] = useState('')
   const [newTaskTitle, setNewTaskTitle] = useState('')
   const itemLabelRefs: { [key: string]: HTMLInputElement | null } = {}
 
@@ -35,33 +31,6 @@ export const TodosList = (): JSX.Element => {
   const handleNewTaskInputChange = useCallback(event => {
     setNewTaskTitle(event.target.value)
   }, [])
-
-  const handleItemClick = useCallback(event => {
-    event.preventDefault()
-  }, [])
-
-  const handleItemDoubleClick = useCallback(
-    (event, id, text) => {
-      setEditableItemId(id)
-      setEditableInitialValue(text)
-    },
-    [itemLabelRefs, editableItemId]
-  )
-
-  const handleKeyDown = useCallback(
-    (event: KeyboardEvent<HTMLInputElement>, id: string) => {
-      if (event.key === 'Escape') {
-        changeSyncMapById(client, tasksStore, id, {
-          text: editableInitialValue
-        })
-
-        setEditableItemId('')
-      } else if (event.key === 'Enter') {
-        setEditableItemId('')
-      }
-    },
-    [editableItemId, client]
-  )
 
   const handleItemOutsideClick = useCallback(
     event => {
@@ -122,72 +91,11 @@ export const TodosList = (): JSX.Element => {
       ) : (
         <ul className={styles.list}>
           {tasks.list.map(todo => (
-            <li
-              className={cn(
-                styles.listItem,
-                todo.id === editableItemId && styles.listItemEditable
-              )}
-              key={todo.id}
-            >
-              <div
-                className={cn(
-                  styles.note,
-                  todo.completed && styles.noteCompleted
-                )}
-              >
-                <input
-                  className={styles.checkbox}
-                  type="checkbox"
-                  id={`todo-${todo.id}`}
-                  onChange={event => {
-                    changeSyncMapById(client, tasksStore, todo.id, {
-                      completed: Boolean(event.target.checked)
-                    })
-                  }}
-                  checked={todo.completed}
-                />
-                <label
-                  className={styles.label}
-                  htmlFor={`todo-${todo.id}`}
-                  onClick={event => {
-                    handleItemClick(event)
-                  }}
-                  onDoubleClick={event => {
-                    handleItemDoubleClick(event, todo.id, todo.text)
-                  }}
-                >
-                  {todo.text}
-                </label>
-                <button
-                  className={styles.deleteControl}
-                  type="button"
-                  onClick={() => {
-                    deleteSyncMapById(client, tasksStore, todo.id)
-                  }}
-                >
-                  Delete task
-                </button>
-              </div>
-              <TextInput
-                id="create-new-task"
-                label="Create new task"
-                placeholder="What needs to be done?"
-                theme="default"
-                className={styles.textInput}
-                value={todo.text}
-                ref={ref => {
-                  itemLabelRefs[todo.id] = ref
-                }}
-                onKeyDown={event => {
-                  handleKeyDown(event, todo.id)
-                }}
-                onChange={event => {
-                  changeSyncMapById(client, tasksStore, todo.id, {
-                    text: event.target.value
-                  })
-                }}
-              />
-            </li>
+            <TodosListItem
+              id={todo.id}
+              completed={todo.completed}
+              text={todo.text}
+            />
           ))}
         </ul>
       )}
---
+++ client/src/components/TodosList/TodosListItem.tsx
@@ -0,0 +1,129 @@
+import cn from 'classnames'
+import { useClient } from '@logux/client/react'
+import { changeSyncMapById, deleteSyncMapById } from '@logux/client'
+import { useCallback, useEffect, useState, KeyboardEvent } from 'react'
+
+import { TextInput } from '../TextInput/TextInput'
+import { tasksStore } from '../../stores/tasks'
+import styles from './TodosList.module.css'
+
+type Props = {
+  id: string
+  completed: boolean
+  text: string
+}
+
+export const TodosListItem = ({ id, completed, text }: Props): JSX.Element => {
+  const client = useClient()
+  const [editableItemId, setEditableItemId] = useState('')
+  const [editableInitialValue, setEditableInitialValue] = useState('')
+  const itemLabelRefs: { [key: string]: HTMLInputElement | null } = {}
+
+  const handleItemClick = useCallback(event => {
+    event.preventDefault()
+  }, [])
+
+  const handleItemDoubleClick = useCallback(
+    (event, todoId, todoText) => {
+      setEditableItemId(todoId)
+      setEditableInitialValue(todoText)
+    },
+    [itemLabelRefs, editableItemId]
+  )
+
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent<HTMLInputElement>, todoId: string) => {
+      if (event.key === 'Escape') {
+        changeSyncMapById(client, tasksStore, todoId, {
+          text: editableInitialValue
+        })
+
+        setEditableItemId('')
+      } else if (event.key === 'Enter') {
+        setEditableItemId('')
+      }
+    },
+    [editableItemId, client]
+  )
+
+  const handleItemOutsideClick = useCallback(
+    event => {
+      if (event.target === itemLabelRefs[editableItemId]) return
+
+      setEditableItemId('')
+    },
+    [editableItemId]
+  )
+
+  useEffect(() => {
+    document.addEventListener('click', handleItemOutsideClick)
+
+    return () => {
+      document.removeEventListener('click', handleItemOutsideClick)
+    }
+  }, [editableItemId])
+
+  return (
+    <li
+      className={cn(
+        styles.listItem,
+        id === editableItemId && styles.listItemEditable
+      )}
+      key={id}
+    >
+      <div className={cn(styles.note, completed && styles.noteCompleted)}>
+        <input
+          className={styles.checkbox}
+          type="checkbox"
+          id={`todo-${id}`}
+          onChange={event => {
+            changeSyncMapById(client, tasksStore, id, {
+              completed: Boolean(event.target.checked)
+            })
+          }}
+          checked={completed}
+        />
+        <label
+          className={styles.label}
+          htmlFor={`todo-${id}`}
+          onClick={event => {
+            handleItemClick(event)
+          }}
+          onDoubleClick={event => {
+            handleItemDoubleClick(event, id, text)
+          }}
+        >
+          {text}
+        </label>
+        <button
+          className={styles.deleteControl}
+          type="button"
+          onClick={() => {
+            deleteSyncMapById(client, tasksStore, id)
+          }}
+        >
+          Delete task
+        </button>
+      </div>
+      <TextInput
+        id="create-new-task"
+        label="Create new task"
+        placeholder="What needs to be done?"
+        theme="default"
+        className={styles.textInput}
+        value={text}
+        ref={ref => {
+          itemLabelRefs[id] = ref
+        }}
+        onKeyDown={event => {
+          handleKeyDown(event, id)
+        }}
+        onChange={event => {
+          changeSyncMapById(client, tasksStore, id, {
+            text: event.target.value
+          })
+        }}
+      />
+    </li>
+  )
+}

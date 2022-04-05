cda604ecb54c8ec6e9bfbc4ed66eca38086260c6
Yuri Mikhin <urasergeevich@mail.ru>
2022-04-05 11:54:57 +0300

    Separate repository into pnpm workspaces.

---
+++ client/postcss.config.js
@@ -0,0 +1,5 @@
+/* eslint-disable node/global-require, node/no-unpublished-require */
+
+module.exports = {
+  plugins: [require('postcss-normalize'), require('postcss-nested')]
+}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'client',
   css: {
     modules: {
       localsConvention: 'camelCase'

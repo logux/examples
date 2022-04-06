cf220328598fed712a2400ea4530fde1191d1aa3
Yuri Mikhin <urasergeevich@mail.ru>
2022-04-06 13:45:52 +0300

    Update @logux/client, react, react-dom.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import { App } from './App.js'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const container = document.getElementById('root')
+
+if (container) {
+  const root = createRoot(container)
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}

500af5990221053e03764d1ddd2d9b6d586712f4
Yuri Mikhin <urasergeevich@mail.ru>
2022-04-07 10:36:04 +0300

    Improve redirects.

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,6 +1,5 @@
 import { useStore } from '@nanostores/react'
-import { useLocation } from 'wouter'
-import { useEffect } from 'react'
+import { Redirect } from 'wouter'
 
 import { AuthForm } from '../components/AuthForm/AuthForm.js'
 import { authStore } from '../stores/auth.js'
@@ -8,13 +7,5 @@ import { authStore } from '../stores/auth.js'
 export const AuthPage = (): JSX.Element => {
   const { id: userId } = useStore(authStore)
 
-  const [, setLocation] = useLocation()
-
-  useEffect(() => {
-    if (userId) {
-      setLocation('/')
-    }
-  }, [userId])
-
-  return <AuthForm />
+  return userId ? <Redirect to="/" /> : <AuthForm />
 }
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,6 +1,5 @@
-import { useLocation } from 'wouter'
+import { Redirect } from 'wouter'
 import { useStore } from '@nanostores/react'
-import { useEffect } from 'react'
 
 import { TodosList } from '../components/TodosList/TodosList.js'
 import { authStore } from '../stores/auth.js'
@@ -8,13 +7,5 @@ import { authStore } from '../stores/auth.js'
 export const MainPage = (): JSX.Element => {
   const { id: userId } = useStore(authStore)
 
-  const [, setLocation] = useLocation()
-
-  useEffect(() => {
-    if (!userId) {
-      setLocation('/auth')
-    }
-  }, [userId])
-
-  return <TodosList />
+  return userId ? <TodosList /> : <Redirect to="/auth" />
 }

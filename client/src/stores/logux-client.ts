6d3144f0eef5fffac56ccf05b70a7cc9890a22eb
Andrey Sitnik <andrey@sitnik.ru>
2023-02-06 23:43:38 +0100

    Simplify Logux client store

--- client/src/stores/logux-client.ts
+++ client/src/stores/logux-client.ts
@@ -1,59 +1,29 @@
 import { badge, badgeEn, confirm, CrossTabClient, log } from '@logux/client'
 import { badgeStyles } from '@logux/client/badge/styles'
-import { atom, onMount } from 'nanostores'
 import { Client } from '@logux/client/client'
+import { atom } from 'nanostores'
 
 import { subprotocol } from '../../../api/index.js'
-import { authStore, logout } from './auth.js'
-
-export const clientStore = atom<Client>()
+import { logout } from './auth.js'
 
 const loguxServer = `${
   window.location.protocol.endsWith('s') ? 'wss' : 'ws'
 }://${window.location.host}/logux`
 
-onMount(clientStore, () => {
-  const client = new CrossTabClient({
-    subprotocol,
-    userId: '',
-    server: loguxServer
-  })
+let client = new CrossTabClient({
+  subprotocol,
+  userId: '',
+  server: loguxServer
+})
 
-  badge(client, { messages: badgeEn, styles: badgeStyles })
-  log(client)
-  confirm(client)
+badge(client, { messages: badgeEn, styles: badgeStyles })
+log(client)
+confirm(client)
 
-  let started = false
-
-  clientStore.set(client)
-
-  let authUnsubscribe = authStore.subscribe(({ id }, changedKey) => {
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (!changedKey || changedKey === 'id') {
-      if (id) {
-        client.changeUser(id)
-
-        if (started) {
-          client.node.connection.connect()
-        } else {
-          client.start()
-          started = true
-        }
-      } else if (client.node.connected) {
-        client.node.connection.disconnect()
-        client.changeUser('')
-      }
-    }
-  })
-
-  client.node.catch(error => {
-    if (error.type === 'wrong-credentials') {
-      logout()
-    }
-  })
-
-  return () => {
-    authUnsubscribe()
-    client.destroy()
+client.node.catch(error => {
+  if (error.type === 'wrong-credentials') {
+    logout()
   }
 })
+
+export const clientStore = atom<Client>(client)

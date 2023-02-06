340c7a6e6887bd243e06fb7a7ed2a2565bfb3ab5
Andrey Sitnik <andrey@sitnik.ru>
2023-02-07 00:06:17 +0100

    Another way to fix client store

--- client/src/stores/logux-client.ts
+++ client/src/stores/logux-client.ts
@@ -1,29 +1,66 @@
 import { badge, badgeEn, confirm, CrossTabClient, log } from '@logux/client'
 import { badgeStyles } from '@logux/client/badge/styles'
+import { atom, onMount } from 'nanostores'
 import { Client } from '@logux/client/client'
-import { atom } from 'nanostores'
 
 import { subprotocol } from '../../../api/index.js'
-import { logout } from './auth.js'
+import { authStore, logout } from './auth.js'
 
 const loguxServer = `${
   window.location.protocol.endsWith('s') ? 'wss' : 'ws'
 }://${window.location.host}/logux`
 
-let client = new CrossTabClient({
+const fakeClient = new CrossTabClient({
   subprotocol,
   userId: '',
-  server: loguxServer
+  server: loguxServer,
+  allowDangerousProtocol: true
 })
 
-badge(client, { messages: badgeEn, styles: badgeStyles })
-log(client)
-confirm(client)
+export const clientStore = atom<Client>(fakeClient)
 
-client.node.catch(error => {
-  if (error.type === 'wrong-credentials') {
-    logout()
+onMount(clientStore, () => {
+  const client = new CrossTabClient({
+    subprotocol,
+    userId: '',
+    server: loguxServer,
+    allowDangerousProtocol: true
+  })
+
+  badge(client, { messages: badgeEn, styles: badgeStyles })
+  log(client)
+  confirm(client)
+
+  let started = false
+
+  clientStore.set(client)
+
+  let authUnsubscribe = authStore.subscribe(({ id }, changedKey) => {
+    if (!changedKey) {
+      if (id) {
+        client.changeUser(id)
+
+        if (started) {
+          client.node.connection.connect()
+        } else {
+          client.start()
+          started = true
+        }
+      } else if (client.node.connected) {
+        client.node.connection.disconnect()
+        client.changeUser('')
+      }
+    }
+  })
+
+  client.node.catch(error => {
+    if (error.type === 'wrong-credentials') {
+      logout()
+    }
+  })
+
+  return () => {
+    authUnsubscribe()
+    client.destroy()
   }
 })
-
-export const clientStore = atom<Client>(client)
